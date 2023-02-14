c0e1fb9d26b72da939a731883dc62128ae0a51a1
Daniel Brott <danbrott42@gmail.com>
2023-02-14 02:06:35 +0000

    button fix

--- cookie-stand-admin/components/Header.js
+++ cookie-stand-admin/components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
       </div>
       <div className="flex-grow"></div>
       <div className="flex items-center justify-end m-5 ">
-        <button type="button" className="bg-silver border border-black hover:bg-metal py-2 px-4 rounded-sm">
+        <button type="button" className="bg-silver border border-black hover:bg-metal py-2 px-4 rounded-xl">
           Overview
         </button>
       </div>
